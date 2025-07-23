d3ddb4e22a1db518599d048dacbadea36b285444
denial-E <shempeter221992@gmail.com>
2025-07-24 00:54:11 +0530

    run bro

--- Routers/user.router.js
+++ Routers/user.router.js
@@ -3,8 +3,8 @@ import { loginUser, registerUser } from '../controllers/user.controller.js';
 
 const router = express.Router()
 
-router.post('/register',registerUser)
-router.post('/login',loginUser)
+router.post('/register',registerUser)//http://localhost:4000/api/register
+router.post('/login',loginUser)//http://localhost:4000/api/login
 
 
 
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,39 +1,34 @@
 import User from "../Models/user.schema.js";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
 export const registerUser = async (req, res) => {
-    try {
-
-        
-        const { username, email, password } = req.body
-        const hashPassword = await bcrypt.hash(password, 10)
-        console.log("hashPassword",hashPassword);
-        const newUser = new User({ username, email, password: hashPassword });
-        await newUser.save()
-        res.status(200).json({messege:"Register Successful", data:newUser})
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error:'Register Failed,Internal server error'})
-    }
-    
-}
+  try {
+    const { username, email, password } = req.body;
+    const hashPassword = await bcrypt.hash(password, 10);
+    console.log("hashPassword", hashPassword);
+    const newUser = new User({ username, email, password: hashPassword });
+    await newUser.save();
+    res.status(200).json({ messege: "Register Successful", data: newUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Register Failed,Internal server error" });
+  }
+};
 
 export const loginUser = async (req, res) => {
-    
-    try {
-        const {email,password}=req.body
-        const user = await User.findOne({ email })
-        if (!user) {
-            return res.status(401).json({messege:"User Not Found"})
-        }
-        const passwordMatch = await bcrypt.compare(password, user.password)
-        if (!passwordMatch) {
-            return res.status(401).json({messege:"Invalid password"})
-        }
-          res.status(200).json({messege:"Login Successful"})
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "login Failed,Internal server error" })
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ messege: "User Not Found" });
     }
-}
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ messege: "Invalid password" });
+    }
+    res.status(200).json({ messege: "Login Successful" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "login Failed,Internal server error" });
+  }
+};
